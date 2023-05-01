33c6dbcb82f2c16bb077aa7806db9fde91ccd6b4
lvoswald <l.v.oswald97@gmail.com>
2023-05-01 16:29:13 +0200

    Reformatting everything into Express Views and further

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,35 +1,10 @@
-express=require("express"),
-app= express();
+exports.getHomepage = (req,res) => {
+    res.render("index");
+}
 
-app.get("/", (req, res) => {
-    let index = req.params.index;
-    res.send(`This is the page for ${veg}`);
-});
-app.get("/items/:vegetable", (req, res) => {
-    let veg = req.params.vegetable;
-    res.send(`This is the page for ${veg}`);
-});
-app.use((req, res, next) => {
-    console.log(`request made to: ${req.url}`);
-    console.log(req.query);
-    next();
-});
-app.use(
-    express.urlencoded({
-        extended: false
-    })
-);
-app.use(express.json());
-app.post("/", (req, res) => {
-    console.log(req.body);
-    console.log(req.query);
-    res.send("POST Successful!");
-});
-
-exports.sendReqParam = (req, res) => {
-    let veg = req.params.vegetable;
-    res.send(`This is the page for ${veg}`);
-};
+exports.getAbout= (req,res) => {
+    res.render("about");
+}
 
 exports.respondWithName = (req, res) => {
     let paramsName = req.params.myName;
