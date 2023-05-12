302cdd412f3022ffaa74003f0c4b44d27e309294
lvoswald <l.v.oswald97@gmail.com>
2023-05-12 18:41:21 +0200

    USer Signup working

--- Main.js
+++ Main.js
@@ -2,9 +2,25 @@
     express = require("express"),
     app = express(),
     homeController = require("./controllers/homeController");
+    usersController = require("./controllers/usersController");
     layouts = require("express-ejs-layouts");
     errorController = require("./controllers/errorController");
-app.set("view engine","ejs");
+    mongoose = require("mongoose");
+    mongoose.connect("mongodb://127.0.0.1:27017/FashionPassion",
+        {useNewUrlParser: true});
+    db = mongoose.connection;
+    mongoose.Promise = global.Promise;
+    db.once("open", () => {
+        console.log("Successfully connected to MongoDB using Mongoose!")
+    });
+
+    const User = require("./models/User");
+
+
+
+
+
+    app.set("view engine","ejs");
 app.set("port", process.env.PORT || 3000);
 app.use(layouts);
 app.use(express.static("public"));
@@ -37,6 +53,14 @@ app.get("/error", homeController.getError);
 app.get("/feedback_form", homeController.getFeedback_Form);
 app.get("/login", homeController.getLogin);
 app.get("/thanks", homeController.getThanks);
+app.get("/users", usersController.getAllUsers,
+    (req,res,next) => {
+    console.log(req.data);
+    res.send(req.data);
+    });
+app.get("/userPage", usersController.getUserPage);
+app.post("/signup", usersController.saveUser);
+
 
 app.use(errorController.respondInternalError);
 app.use(errorController.respondNoResourceFound);
