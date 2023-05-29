9f67afc399523b664bd7cb6cd7fdb3c1e4975266
lvoswald <l.v.oswald97@gmail.com>
2023-05-29 16:42:57 +0200

    IMproved the Product and user models with some simple traits and validators.

--- models/User.js
+++ models/User.js
@@ -3,11 +3,45 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     userID: Schema.Types.ObjectId,
-    firstname: String,
-    lastname: String,
-    email: String,
-    password: String
-});
+    name: {
+    firstname: {
+        type: String,
+    required: true,
+    trim: true},
+    lastname: {
+        type: String,
+        required: true,
+    trim: true},
+    },
+    email: {
+        type: String,
+        required: true,
+    lowercase: true,
+    unique: true},
+    password: {
+        type: String,
+        required: true},
+    products: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"}],
+timestamps: true}
 
+
+
+);
+
+userSchema.virtual("fullname")
+    .get(function() {
+        return `${this.name.firstname} ${this.name.lastname}`;
+    });
+userSchema.methods.getInfo = function(){
+    return `First Name: ${this.firstname} Last Name: ${this.lastname} Email: ${this.email}`;
+}
+userSchema.methods.findUserByFirstname = function() {
+    return this.model("User")
+        .find({firstname: this.firstname})
+        .exec();
+};
 const User = mongoose.model("User", userSchema);
 module.exports = mongoose.model("User", userSchema);
+
